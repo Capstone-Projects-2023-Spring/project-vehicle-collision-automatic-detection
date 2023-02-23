5c75118b4b3c696e51842d0e6dd90eda7dc6732d
NathanAdiam <nathan.adiam@temple.edu>
2023-02-22 21:48:29 -0500

    Added a slide-to-delete feature + changed some variable names

--- VC/Controllers/VCContactsViewController.swift
+++ VC/Controllers/VCContactsViewController.swift
@@ -10,7 +10,7 @@ import Contacts
 import ContactsUI
 import SwiftUI
 
-struct Person {
+struct Contact: Identifiable {
     let name: String
     let id: String
     let source: CNContact
@@ -25,7 +25,7 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
         return table
     }()
     
-    var models = [Person]()
+    var EmergencyContantList = [Contact]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,40 +41,52 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
             action: #selector(didTapAdd))
         
     }
-    @objc func didTapAdd() {
-        let viewController = CNContactPickerViewController()
+    
+    @objc func didTapAdd() {        let viewController = CNContactPickerViewController()
         viewController.delegate = self
         present(viewController, animated: true)
     }
     
+    
     func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
         let name = contact.givenName + " " + contact.familyName
         let identifier = contact.identifier
-        let model = Person(name: name,
+        let person = Contact(name: name,
                            id: identifier,
                            source: contact
         )
-        models.append(model)
+        EmergencyContantList.append(person)
         table.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        models.count
+        EmergencyContantList.count
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt  indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            EmergencyContantList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = models[indexPath.row].name
+        cell.textLabel?.text = EmergencyContantList[indexPath.row].name
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let contact = models[indexPath.row].source
+        let contact = EmergencyContantList[indexPath.row].source
         let viewController = CNContactViewController(for: contact)
-        let vc2 = UINavigationController(rootViewController: viewController)
-        present(vc2, animated: true)
-        
+        present(UINavigationController(rootViewController: viewController), animated: true)
     }
 }
