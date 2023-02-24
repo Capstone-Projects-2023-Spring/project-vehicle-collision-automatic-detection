98ed06f6415b2970cc5e58db74abfddf43e6c040
AFoster414 <austinrfoster2001@gmail.com>
2023-02-24 13:33:02 -0500

    Added small functionality to 'add contact' button for hello world

--- androidApp/app/src/main/java/edu/temple/vehiclecollisiondetection/MainActivity.kt
+++ androidApp/app/src/main/java/edu/temple/vehiclecollisiondetection/MainActivity.kt
@@ -1,11 +1,13 @@
 package edu.temple.vehiclecollisiondetection
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -15,16 +17,19 @@ class MainActivity : AppCompatActivity() {
     //recycler view to hold contact list
     lateinit var recyclerView: RecyclerView
     lateinit var connectionText: TextView
+    lateinit var helloWorldText: TextView //added for hello world
 
     //contact data class
     data class ContactObject(val phoneNumber: String, val name: String)
 
+    @SuppressLint("SetTextI18n")//added for hello world
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.contactRecyclerView)
         connectionText = findViewById(R.id.connectionText)
+        helloWorldText = findViewById(R.id.HelloWorldView)
 
         connectionText.setTextColor(Color.parseColor("red"))
 
@@ -34,6 +39,12 @@ class MainActivity : AppCompatActivity() {
         )
 
         recyclerView.adapter = ContactAdapter(contactObjects)
+
+       val addContactButton: View = findViewById(R.id.fab)
+        addContactButton.setOnClickListener{
+            helloWorldText.setText("Hello World!")//added for hello world
+            helloWorldText.setTextColor(Color.parseColor("green"))//added for hello world
+        }
     }
 }
 
