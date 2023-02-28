701b0627cba18605443773bb23de9b0ce408d02f
AFoster414 <austinrfoster2001@gmail.com>
2023-02-27 21:42:11 -0500

    Updated 'add contact' feature, now the user can input a contact name and number and is saved onto contactObject array. still doesnt save tho

--- androidApp/app/src/main/java/edu/temple/vehiclecollisiondetection/MainActivity.kt
+++ androidApp/app/src/main/java/edu/temple/vehiclecollisiondetection/MainActivity.kt
@@ -8,8 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -40,11 +42,34 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.adapter = ContactAdapter(contactObjects)
 
+        //Add Contact Button Functionality
        val addContactButton: View = findViewById(R.id.fab)
         addContactButton.setOnClickListener{
-            contactObjects.add(ContactObject("2152223456", "Tom"))
-            recyclerView.adapter = ContactAdapter(contactObjects)
+            //setting up 'add contact' pop-up menu
+            val contactDialogView = LayoutInflater.from(this).inflate(R.layout.layout_dialog, null)
+            val contactDialogBuilder = AlertDialog.Builder(this)
+                .setView(contactDialogView)
+                .setTitle("Add Contact")
+            //show dialog
+            val contactAlertDialog = contactDialogBuilder.show()
+            //save/confirm button
+            val saveButton = contactDialogView.findViewById<Button>(R.id.save_button)
+            saveButton.setOnClickListener {
+                contactAlertDialog.dismiss()
+                //gets the input information
+                val contactName = contactDialogView.findViewById<EditText>(R.id.contact_name).text.toString()
+                val contactNumber = contactDialogView.findViewById<EditText>(R.id.contact_number).text.toString()
+                //adds contact to the list of contactObjects
+                contactObjects.add(ContactObject(contactNumber, contactName))
+                recyclerView.adapter = ContactAdapter(contactObjects)
+            }
+            //cancel button
+            val cancelButton = contactDialogView.findViewById<Button>(R.id.cancel_button)
+            cancelButton.setOnClickListener {
+                contactAlertDialog.dismiss()
+            }
         }
+
     }
 }
 
