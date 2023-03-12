ba145b17c707139ee8ae734bbbebac8b17e9b378
thanhnguyen46 <phuocthanhnguyen46.com>
2023-03-11 19:09:35 -0500

    Implement BluetoothManager

---
+++ VC/Controllers/BluetoothManager.swift
@@ -0,0 +1,61 @@
+//
+//  BluetoothManager.swift
+//  VC
+//
+//  Created by Thanh Nguyen on 3/11/23.
+//
+
+import CoreBluetooth
+
+class BluetoothManager: NSObject, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager?
+    var peripheral: CBPeripheral?
+    let SERVICE_UUID = CBUUID(string: "SERVICE_UUID")
+    let CHARACTERISTIC_UUID = CBUUID(string: "CHARACTERISTIC_UUID")
+    
+    func start() {
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: [SERVICE_UUID], options: nil)
+        } else {
+            print("Bluetooth not available.")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        self.peripheral = peripheral
+        central.connect(peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices([SERVICE_UUID])
+    }
+}
+
+extension BluetoothManager: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            peripheral.discoverCharacteristics([CHARACTERISTIC_UUID], for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+        for characteristic in characteristics {
+            if characteristic.uuid == CHARACTERISTIC_UUID {
+                peripheral.readValue(for: characteristic)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let value = characteristic.value {
+            // Handle the retrieved value
+        }
+    }
+}
