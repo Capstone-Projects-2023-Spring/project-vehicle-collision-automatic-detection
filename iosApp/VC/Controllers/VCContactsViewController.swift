e8bbe83e6ac843931c171dff38dfba65d2eb97e0
thanhnguyen46 <phuocthanhnguyen46.com>
2023-03-18 23:34:54 -0400

    Fix Spelling in EmergencyContactList

--- iosApp/VC/Controllers/VCContactsViewController.swift
+++ iosApp/VC/Controllers/VCContactsViewController.swift
@@ -14,10 +14,10 @@ import CoreData
 class VCContactsViewController: UIViewController, UITableViewDataSource, CNContactPickerDelegate, UITableViewDelegate {
     
     /**
-         Creates a table to store the emergency contacts
-
-        - Returns: The table made
-         */
+     Creates a table to store the emergency contacts
+     
+     - Returns: The table made
+     */
     private let table: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self,
@@ -25,9 +25,9 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
         return table
     }()
     
-    var EmergencyContantList = [Contact]()
+    var EmergencyContactList = [Contact]()
     /**
-      This method is called after the view controller has loaded its view hierarchy into memory.
+     This method is called after the view controller has loaded its view hierarchy into memory.
      */
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,13 +52,13 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
     }
     
     /**
-         Checks if a contact has been selected by the user
-
-         - Parameters:
-            - CNContactPickerViewController: The default iOS contact application
-            - Contact: A specific contact from the contact app
+     Checks if a contact has been selected by the user
      
-         */
+     - Parameters:
+     - CNContactPickerViewController: The default iOS contact application
+     - Contact: A specific contact from the contact app
+     
+     */
     func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
         let name = contact.givenName + " " + contact.familyName
         let identifier = contact.identifier
@@ -67,53 +67,53 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
         newContact.setValue(name, forKey: #keyPath(Contact.contactName))
         newContact.setValue(identifier, forKey: #keyPath(Contact.contactId))
         newContact.setValue(contact, forKey: #keyPath(Contact.contactSource))
-        self.EmergencyContantList.insert(newContact, at: 0)
+        self.EmergencyContactList.insert(newContact, at: 0)
         AppDelegate.sharedAppDelegate.CoreDataStack.saveContext() // Save changes in CoreData
         DispatchQueue.main.async {
             self.table.reloadData()
         }
-    
+        
     }
     
     /**
-         Checks if row can be editted
-
-         - Parameters:
-            - UITableView: The table view
-            - Section: The section of the emergency contacts
+     Checks if row can be editted
      
-        - Returns: The count of Emergency contacts added
-         */
+     - Parameters:
+     - UITableView: The table view
+     - Section: The section of the emergency contacts
+     
+     - Returns: The count of Emergency contacts added
+     */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        EmergencyContantList.count
+        EmergencyContactList.count
     }
     
     /**
-         Checks if row can be edited
-
-         - Parameters:
-            - UITableView: The table view
-            - IndexPath: The row at index path
+     Checks if row can be edited
      
-        - Returns: If it's possible to edit a specific row
-         */
+     - Parameters:
+     - UITableView: The table view
+     - IndexPath: The row at index path
+     
+     - Returns: If it's possible to edit a specific row
+     */
     func tableView(_ tableView: UITableView, canEditRowAt  indexPath: IndexPath) -> Bool {
         return true
     }
     
     /**
-         Allows to slide row to delete Emergency contacts
-
-         - Parameters:
-            - UITableView: The table view
-            - UITableViewCell.EditingStyle: The editing style
-            - IndexPath: The row at index path
-         */
+     Allows to slide row to delete Emergency contacts
+     
+     - Parameters:
+     - UITableView: The table view
+     - UITableViewCell.EditingStyle: The editing style
+     - IndexPath: The row at index path
+     */
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Remove the contact from the CoreData
-            AppDelegate.sharedAppDelegate.CoreDataStack.managedContext.delete(EmergencyContantList[indexPath.row])
-            EmergencyContantList.remove(at: indexPath.row)
+            AppDelegate.sharedAppDelegate.CoreDataStack.managedContext.delete(EmergencyContactList[indexPath.row])
+            EmergencyContactList.remove(at: indexPath.row)
             // Save Changes
             AppDelegate.sharedAppDelegate.CoreDataStack.saveContext()
             // Remove row from TableView
@@ -124,42 +124,43 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
     }
     
     /**
-         Finds the cell at a given row
-
-         - Parameters:
-            - UITableView: The table view
-            - IndexPath: The indexPath of the row
-            - UITableViewCell: The UITableViewCell
-
-        - Returns: A cell for a given row
-         */
+     Finds the cell at a given row
+     
+     - Parameters:
+     - UITableView: The table view
+     - IndexPath: The indexPath of the row
+     - UITableViewCell: The UITableViewCell
+     
+     - Returns: A cell for a given row
+     */
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = EmergencyContantList[indexPath.row].contactName
+        cell.textLabel?.text = EmergencyContactList[indexPath.row].contactName
         return cell
     }
     
     /**
-         Presents the selected contacts
-
-         - Parameters:
-            - UITableView: The table view
-            - IndexPath: The indexPath of the row
-         */
+     Presents the selected contacts
+     
+     - Parameters:
+     - UITableView: The table view
+     - IndexPath: The indexPath of the row
+     */
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        guard let contact = EmergencyContantList[indexPath.row].contactSource else { return }
+        guard let contact = EmergencyContactList[indexPath.row].contactSource else { return }
         let viewController = CNContactViewController(for: contact)
         present(UINavigationController(rootViewController: viewController), animated: true)
     }
+
     /**
-         Fetches data. This method gets the list of Contacts from the CoreData using NSFetchRequest. Called everytime we open the app
-
-         - Parameters:
-
-        - Returns: Contacts Data from CoreData
-         */
+     Fetches data. This method gets the list of Contacts from the CoreData using NSFetchRequest. Called everytime we open the app
+     
+     - Parameters:
+     
+     - Returns: Contacts Data from CoreData
+     */
     private func getContacts() {
         let contactFetch: NSFetchRequest<Contact> = Contact.fetchRequest()
         let sortByName = NSSortDescriptor(key: #keyPath(Contact.contactName), ascending: false)
@@ -167,7 +168,7 @@ class VCContactsViewController: UIViewController, UITableViewDataSource, CNConta
         do {
             let managedContext = AppDelegate.sharedAppDelegate.CoreDataStack.managedContext
             let results = try managedContext.fetch(contactFetch)
-            EmergencyContantList = results
+            EmergencyContactList = results
         } catch let error as NSError {
             print("Fetch error: \(error) description: \(error.userInfo)")
         }
