afc28a17b4a05f61ddef2bcabeb038af11f72e68
Thanh Nguyen <phuocthanhnguyen46@gmail.com>
2023-03-28 21:59:06 -0400

    Add a button to test Twilio API Call

--- iosApp/VC/Controllers/VCSettingViewController.swift
+++ iosApp/VC/Controllers/VCSettingViewController.swift
@@ -11,13 +11,21 @@ import MobileCoreServices
 
 /// Controller to view and change the application's settings
 final class VCSettingViewController: UIViewController {
-
+    @objc func sendMessageButtonTapped() {
+        let vcContacts = VCContactsViewController()
+        vcContacts.textMessageWithTwilio()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         title = "Settings"
         // Do any additional setup after loading the view.
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        button.center = view.center
+        button.setTitle("Test MSG", for: .normal)
+        button.addTarget(self, action: #selector(sendMessageButtonTapped), for: .touchUpInside)
+        view.addSubview(button)
     }
-    
 }
