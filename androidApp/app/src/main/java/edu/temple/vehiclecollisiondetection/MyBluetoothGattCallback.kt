a19222ca86e35c21a562369327983356e9aedcbf
AFoster414 <austinrfoster2001@gmail.com>
2023-03-31 16:17:14 -0400

    Revert "Revert "Revert "Revert "Revert "Revert "Migrated bluetooth thread and callback into separate classes (untested)""""""
    This reverts commit 835e8350c424f15838dc16300e5899d18698fcb8.

---
+++ androidApp/app/src/main/java/edu/temple/vehiclecollisiondetection/MyBluetoothGattCallback.kt
@@ -0,0 +1,116 @@
+package edu.temple.vehiclecollisiondetection
+
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.*
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import java.util.*
+
+class MyBluetoothGattCallback(currentContext: Context, currentActivity: Activity, connectionText: TextView) : BluetoothGattCallback() {
+    val activeContext = currentContext
+    val activeActivity = currentActivity
+    val connectionStatusText = connectionText
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+        if (ActivityCompat.checkSelfPermission(
+                activeContext,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            activeActivity.requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 10)
+            //return
+        }
+        if (newState == BluetoothProfile.STATE_CONNECTED) {
+            Log.d("tag1", "Connection Succeeded!")
+            activeActivity.runOnUiThread(Runnable() {
+                Toast.makeText(activeContext, "Device Connected!", Toast.LENGTH_LONG).show()
+                connectionStatusText.setTextColor(Color.parseColor("green"))
+                connectionStatusText.setText("Connected!")
+            })
+            gatt.discoverServices()
+        } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+            // handle disconnection
+            Log.d("tag2", "Connection Disconnected!")
+            activeActivity.runOnUiThread(Runnable() {
+                connectionStatusText.setTextColor(Color.parseColor("red"))
+                connectionStatusText.setText("Not Connected")
+                Toast.makeText(activeContext, "Device Disconnected!", Toast.LENGTH_LONG).show()
+            })
+        } else{
+            Log.d("tag3", "Connection Attempt Failed!")
+            gatt.close()
+        }
+    }
+
+    @RequiresApi(33)
+    override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+        val serviceUuid = UUID.fromString("00110011-4455-6677-8899-aabbccddeeff")//acts like a 'password' for the bluetooth connection
+        val characteristicUuid = UUID.fromString("00112233-4455-6677-8899-abbccddeefff")//acts like a 'password' for the bluetooth connection
+        if (ActivityCompat.checkSelfPermission(
+                activeContext,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            activeActivity.requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 10)
+            //return
+        }
+        if (status == BluetoothGatt.GATT_SUCCESS) {
+            val service1 = gatt.getService(serviceUuid)
+            val characteristic1 = service1.getCharacteristic(characteristicUuid)//characteristic uuid here
+            if(service1 == null){
+                Log.d("Invalid service:", "service is null!")
+                Log.d("Service UUid:", serviceUuid.toString())
+            }else{
+                Log.d("Service Found:", serviceUuid.toString())
+            }
+            if(characteristic1 == null){
+                Log.d("Invalid characteristic:", "characteristic is null!")
+                Log.d("Characteristic UUid:", characteristicUuid.toString())
+            }else{
+                Log.d("Characteristic Found:", characteristicUuid.toString())
+            }
+            gatt.setCharacteristicNotification(characteristic1, true)
+
+            val desc: BluetoothGattDescriptor = characteristic1.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"))
+            Log.d("Descriptor Found:", "00002902-0000-1000-8000-00805f9b34fb")
+            gatt.writeDescriptor(desc, BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE)
+        }
+    }
+
+    override fun onCharacteristicChanged(
+        gatt: BluetoothGatt,
+        characteristic: BluetoothGattCharacteristic,
+        value: ByteArray
+    ) {
+        // handle received data
+        Log.d("Characteristic Data", "Data Changed!")
+        val data = String(value)
+        if(data == "F") {
+            //if a crash is detected by the arduino device, initiate crash popup
+            val crashDialogView = LayoutInflater.from(activeContext).inflate(R.layout.crash_procedure_popup, null)
+            val crashDialogBuilder = AlertDialog.Builder(activeContext)
+                .setView(crashDialogView)
+                .setTitle("")
+            //show dialog
+            val crashAlertDialog = crashDialogBuilder.show()
+            //cancel button
+            val cancelButton = crashDialogView.findViewById<Button>(R.id.crash_cancel_button)
+            cancelButton.setOnClickListener {
+                crashAlertDialog.dismiss()
+            }
+        }
+    }
+
+}
