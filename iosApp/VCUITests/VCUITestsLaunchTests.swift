581786356c0a46e0fe42e374559f5f9e927e2875
Thanh Nguyen <thanh.p.nguyen46@gmail.com>
2023-04-22 17:20:38 -0400

    Add Working UI Tests

---
+++ iosApp/VCUITests/VCUITestsLaunchTests.swift
@@ -0,0 +1,31 @@
+//
+//  VCUITestsLaunchTests.swift
+//  VCUITests
+//
+//  Created by Thanh Nguyen on 4/22/23.
+//
+
+import XCTest
+
+final class VCUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
