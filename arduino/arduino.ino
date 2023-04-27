a9532fc86a49e9d423f91907badf82bc9922390d
Evan Noyes <evan.noyes@temple.edu>
2023-04-26 21:43:18 -0400

    bug fixes, combined accelerometer tests into seperate file

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,19 +10,27 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "BluefruitConfig.h"
+#include "accelerometer.h"
+#include "SparkFun_LIS331.h"
 
 #define FACTORYRESET_ENABLE         1
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 #define MODE_LED_BEHAVIOUR          "MODE"
 
+LIS331 xl;
+float scale = 100.0;
+float threshold = 10.0;
+
 String data = " ";
 int flag = 0;
-int sleep = 0;
 int start = 0;
-const unsigned long interval = 60000;
+const unsigned long interval = 30000;
 unsigned long previousMillis = 0;
+uint8_t signal0[] = {0x48};
+uint8_t signal1[] = {0x70};
 
 Adafruit_BluefruitLE_SPI Bluetooth(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+Adafruit_BLEGatt gatt(Bluetooth);
 
 /**
  * @brief Initialize input, output pins and values
@@ -33,8 +41,10 @@ Adafruit_BluefruitLE_SPI Bluetooth(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUI
  */
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  
+  /* Put the device into idle mode */
+  LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_ON, SPI_OFF, USART0_OFF, TWI_OFF);
 
   /* Initialise the module */
   Serial.print(F("Initialising the Bluefruit LE module: "));
@@ -57,10 +67,32 @@ void setup() {
 
   Bluetooth.verbose(false);
 
+  // Create custom characteristic
+  Serial.println(F("Setting service + characteristic!"));
+  Bluetooth.atcommand("AT+GATTADDSERVICE=UUID128=00-11-00-11-44-55-66-77-88-99-AA-BB-CC-DD-EE-FF");
+
+  int char1 = Bluetooth.atcommand("AT+GATTADDCHAR=UUID128=00-11-22-33-44-55-66-77-88-99-AB-BC-CD-DE-EF-FF,PROPERTIES=0x10,MIN_LEN=1,VALUE=HELLO");
+  Serial.println(char1);
+
+  // Reset Bluefruit for custom characteristic
+  Bluetooth.reset();
+
   /* Wait for connection */
   Serial.println("Looking for Bluetooth Device...");
   while (!Bluetooth.isConnected()){
-    delay(500);    
+    unsigned long currentMillis = millis();
+    // Power down if 30 seconds of no connection passes
+    if((unsigned long)(currentMillis - previousMillis) >= interval){
+      Serial.println("Powering Down!");
+      // Make the Bluetooth undiscoverable
+      Bluetooth.setAdvData(NULL, 0);
+      // Turn off the Mode LED
+      Bluetooth.sendCommandCheckOK("AT+HWModeLED=0");
+      delay(500);
+      // Power down the device
+      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    }
+    delay(500);
   }
 
   // Change Mode LED Activity
@@ -75,6 +107,53 @@ void setup() {
   Serial.println(F("Bluetooth Device Connected!"));
   Serial.println(F("******************************"));
   start++;
+
+  delay(500);
+
+  //accelerometer
+  pinMode(9,INPUT);       // Interrupt pin input
+  pinMode(10, OUTPUT);    // CS for SPI
+  digitalWrite(10, HIGH); // Make CS high
+  pinMode(11, OUTPUT);    // MOSI for SPI
+  pinMode(12, INPUT);     // MISO for SPI
+  pinMode(13, OUTPUT);    // SCK for SPI
+  SPI.begin();
+  xl.setSPICSPin(10);     // This MUST be called BEFORE .begin() so 
+                          //  .begin() can communicate with the chip
+  xl.begin(LIS331::USE_SPI); // Selects the bus to be used and sets
+                          //  the power up bit on the accelerometer.
+                          //  Also zeroes out all accelerometer
+                          //  registers that are user writable.
+  // This next section configures an interrupt. It will cause pin
+  //  INT1 on the accelerometer to go high when the absolute value
+  //  of the reading on the Z-axis exceeds a certain level for a
+  //  certain number of samples.
+  xl.intSrcConfig(LIS331::INT_SRC, 1); // Select the source of the
+                          //  signal which appears on pin INT1. In
+                          //  this case, we want the corresponding
+                          //  interrupt's status to appear. 
+  xl.setIntDuration(1, 1); // Number of samples a value must meet
+                          //  the interrupt condition before an
+                          //  interrupt signal is issued. At the
+                          //  default rate of 50Hz, this is one sec.
+  float intThresholdG = sqrt(pow(threshold, 2)/2.0); //Minimum G on an individual axis for total to exceed threshold
+  int intThresholdCount = convertToReading(scale, intThresholdG);
+  xl.setIntThreshold(intThresholdCount/16, 1); // Threshold for an interrupt. This is
+                          //  not actual counts, but rather, actual
+                          //  counts divided by 16.
+  xl.enableInterrupt(LIS331::X_AXIS, LIS331::TRIG_ON_HIGH, 1, true);  
+  xl.enableInterrupt(LIS331::Y_AXIS, LIS331::TRIG_ON_HIGH, 1, true);
+  xl.enableInterrupt(LIS331::Z_AXIS, LIS331::TRIG_ON_HIGH, 1, true);
+                          // Enable the interrupt. Parameters indicate
+                        //  which axis to sample, when to trigger
+                          //  (in this case, when the absolute mag
+                          //  of the signal exceeds the threshold),
+                          //  which interrupt source we're configuring,
+                          //  and whether to enable (true) or disable
+                          //  (false) the interrupt.
+  xl.setPowerMode(LIS331::NORMAL);
+  xl.setODR(LIS331::DR_50HZ);
+  xl.setFullScale(LIS331::LOW_RANGE);
 }
 
 /**
@@ -88,15 +167,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
   //check if bluetooth device is connected to hardware
   while(Bluetooth.isConnected()){
     unsigned long currentMillis = millis();
 
+    //put device into idle mode
+    LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_ON, SPI_OFF, USART0_OFF, TWI_OFF);
+
     //reset sleep timer for first connection
     if(start == 1){
       previousMillis = currentMillis;
+      //initialize characteristic value to '0'
+      gatt.setChar(1, signal0, sizeof(signal0));
       start--;
+      delay(500);
     }
 
     //reset sleep timer for reconnection
@@ -104,33 +189,47 @@ void loop() {
       Serial.println(F("******************************"));
       Serial.println(F("Bluetooth Device Connected!"));
       Serial.println(F("******************************"));
-      flag--;
       previousMillis = currentMillis;
-    }
-
-    //sleep if a minute of inactivity passes
-    if((unsigned long)(currentMillis - previousMillis) >= interval && (unsigned long)(currentMillis - previousMillis) < interval * 5){
-      if(sleep == 0){
-        Serial.println("Going to Sleep!");
-        sleep++;
-        delay(500);
-        LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-      }
-    }
-
-    //power down if 5 minutes of inactivity passes
-    if((unsigned long)(currentMillis - previousMillis) >= interval * 5){
-      Serial.println("Powering Down!");
+      //initialize characteristic value to '0'
+      gatt.setChar(1, signal0, sizeof(signal0));
+      flag--;
       delay(500);
-      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    }
+
+    float maxG;
+
+    //print max g value
+    if(xl.newXData() || xl.newYData() || xl.newZData()){
+      float maxG = getMaxG();
+      int16_t x, y, z;
+      xl.readAxes(x, y, z);
+      Serial.println(maxG);
+    }
+
+    //checks if threshold might have been exceeded, then verifies
+    if (digitalRead(9) == HIGH && (maxG = getMaxG()) > threshold) { 
+      Serial.println("Interrupt: " + String(maxG) + "g");
+      Serial.println("Changing Characteristic!");
+      //change characteristic value to 'F'
+      gatt.setChar(1, signal1, sizeof(signal1));
+      delay(3000);
+      Serial.println("Resetting Characteristic!");
+      //reset characteristic value back to '0'
+      gatt.setChar(1, signal0, sizeof(signal0));
+      delay(1000);
+      //reset the sleep timer
+      previousMillis = currentMillis;
+    } 
+
+    //disconnect from bluetooth if 10 minutes of inactivity passes
+    if((unsigned long)(currentMillis - previousMillis) >= interval * 20){
+      previousMillis = currentMillis;
+      Bluetooth.disconnect();
+      delay(500);
     }
 
     //check for incoming data from bluetooth device
     if(Bluetooth.available() > 0){
-      if(sleep == 1){
-        Serial.println("Woke Up!");
-        sleep--;
-      }
       data = Bluetooth.readString();
       Serial.print("Bluetooth: " + data); 
       previousMillis = currentMillis;
@@ -138,10 +237,6 @@ void loop() {
 
     //check for incoming data from hardware
     if(Serial.available() > 0){
-      if(sleep == 1){
-        Serial.println("Woke Up!");
-        sleep--;
-      }
       data = Serial.readString();
       Bluetooth.print("Serial: " + data);
       Serial.println("Message sent to bluetooth.");
@@ -151,29 +246,80 @@ void loop() {
 
   //check if hardware disconnects from bluetooth device
   while (!Bluetooth.isConnected()){
+    unsigned long currentMillis = millis();
+
+    //put device into idle mode
+    LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_ON, SPI_OFF, USART0_OFF, TWI_OFF);
+
     if(flag == 0){
-      Serial.println(F("******************************"));
+      Serial.println(F("**"));
       Serial.println("Bluetooth Device Disconnected!");
-      Serial.println(F("******************************"));
+      Serial.println(F("**"));
       flag++;
+      previousMillis = currentMillis;
     }
     delay(500);
+
+    //power down if 30 seconds of no connection passes
+    if((unsigned long)(currentMillis - previousMillis) >= interval){
+      Serial.println("Powering Down!");
+      // Make the Bluetooth undiscoverable
+      Bluetooth.setAdvData(NULL, 0);
+      // Turn off the Mode LED
+      Bluetooth.sendCommandCheckOK("AT+HWModeLED=0");
+      delay(500);
+      // Power down the device
+      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    }
   }
 }
 
 /**
- * @brief Gets accelerometer reading
+ * @brief Error handler for Arduino methods
  * 
- * @param void takes nothing
+ * @param err error message string
  * 
- * @return float returns accelerometer reading
+ * @return void returns nothing
  */
-
-float getAccelerometerRead(){
-  return 0;
-}
 
 void error(const __FlashStringHelper*err){
   Serial.println(err);
   while (1);
+}
+
+/**
+ * @brief Convert sensor reading into an integer value
+ * 
+ * @param maxScale maximum scale for g-force
+ * 
+ * @param g minimum g on individual axis
+ * 
+ * @return integer value of sensor reading
+ */
+
+int convertToReading(int maxScale, float g) {
+  int reading = int((g * 2047) / maxScale);
+  return reading;
+}
+
+/**
+ * @brief Calculates maximum g-force in any direction
+ * 
+ * @param void takes nothing
+ * 
+ * @return maximum g-force
+ */
+
+float getMaxG() {
+  int16_t x, y, z;
+
+  xl.readAxes(x, y, z);
+  
+  float xg = xl.convertToG(scale,x);
+  float yg = xl.convertToG(scale,y);
+  float zg = xl.convertToG(scale,z);
+  
+  float maxG = sqrt(pow(xg, 2) + pow(yg, 2) + pow(zg, 2)); //pythagoream theorem for three dimensions
+
+  return maxG;
 }
